f2c696ada2e203d21c8f0040f3bd74b4c5b2af61
venkatpraveen26 <venkatpraveen26@gmail.com>
2026-02-12 14:33:05 +0530

    Add files via upload
    basic webpage

---
+++ src/App.js
@@ -0,0 +1,364 @@
+import "./App.css"
+
+
+
+
+function App(){
+    return(
+        <div className="document">
+            <div className="corprate">
+                <h1>CORPO<span>RATE</span></h1>
+                <h4> Consequuntur eius minima error quam expedita fugit adipisci </h4>
+            </div>
+
+            <div className="box">
+                <div className="box1">
+                    <h4><i class='bx bxl-graphql'></i></h4>
+                </div>
+                <div className="box2">
+                <h4><i class='bx bx-run' ></i></h4>
+                </div>
+                <div className="box3">
+                <h4><i class='bx bx-book-open'></i></h4>
+                </div>
+                <div className="box4">
+                <h4><i class='bx bx-run' ></i></h4>
+                </div>
+            </div>
+
+            <div className="blog">
+                <p>
+                    <hr style={{height:"7px",backgroundColor:"blue"}}></hr>
+                </p>
+                <h3>After Line</h3>
+                <h1>We Have The Knowledge and Experience</h1>
+                <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ipsam, quis magni aliquid, accusantium natus, unde ex eius quibusdam maxime ipsa. Minus nihil officia repudiandae nulla aperiam, laboriosam rem obcaecati?</h4>
+            </div>
+            {/* end.... */}
+
+            <div className="apple">
+                <div className="appletop">
+                    <div className="part1">
+
+                       <div className="left">
+                        <div className="left1">0 1</div>
+                        <div className="left2"><h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae facere mollitia voluptatum minima ea veniam porro expedita quidem aspernatur quia numquam ratione quaerat vero unde, maiores perferendis reiciendis autem dolorem</h3></div>
+                       
+                       </div>
+
+                       <div className="right">
+
+                       <div className="right1">0 2</div>
+                       <div className="right2"><h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae facere mollitia voluptatum minima ea veniam porro expedita quidem aspernatur quia numquam ratione quaerat vero unde, maiores perferendis reiciendis autem dolorem</h3></div>
+                       </div>
+
+                    </div>
+                    <div className="part2">
+                    <div className="left3">
+                    <img src="https://reactjs.org/logo-og.png" alt="react logo" style={{ width: '550px',height:"400px" }}/>
+                    </div>
+                    <div className="left4">
+                        <h3>aspernatur quia numquam ratione quaerat vero unde, maiores perferendis reiciendis autem dolorem</h3>
+                    </div>
+                    </div>
+                </div>
+
+
+                <div className="applebottom">
+                    <div className="leftbtm">
+                        <h4>head1</h4>
+                        <h1>We run all Kinds of IT 
+                            <br></br>
+                            Services that wow your 
+                            <br></br>
+                            Success
+                        </h1>
+                        <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias nulla soluta unde obcaecati mollitia distinctio veniam eaque quidem voluptatum ipsam ducimus minima dolorem </h5>
+                        <button>Register Now</button>
+                    </div>
+
+                    <div className="rightbtm">
+                        <h3>Heading 1</h3>
+                        <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias nulla soluta undeLorem ipsum dolor sit amet consectetur adipisicing elit. Molestias nulla soluta unde </h5>
+                        <h3>Heading 1</h3>
+                        <h5> Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias nulla soluta undeLorem ipsum dolor sit amet consectetur adipisicing elit. Molestias nulla soluta unde </h5>
+                        <h3>Heading 1</h3>
+                        <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias nulla soluta undeLorem ipsum dolor sit amet consectetur adipisicing elit. Molestias nulla soluta unde </h5>
+                    </div>
+                </div>
+
+            </div>
+
+            <div className="girl-box">
+
+                <div className="girl1">
+                <img src="https://wallpapercave.com/wp/wp5716324.jpg" alt="react logo" style={{ width: '550px',height:"400px" }}/>
+                <h3>Blueeyes....</h3>
+                </div>
+
+                <div className="girl2">
+                <h2>We Belive That people
+                    <br></br>
+                    can be truely happy on
+                    <br></br>
+                    The outside only After<br></br>
+                    Reaching Happiness
+                    <br></br>
+
+                </h2>
+                <br></br>
+                <h3>Cute Anime girl</h3>
+                <h5>Some memories priceless.....................</h5>
+                </div>
+
+            </div>
+
+            <div className="team">
+
+                <div className="team1">
+                
+                <img className="leftteam" src="https://wallpapercave.com/wp/wp5716324.jpg" alt="react logo" style={{ width: '100px',height:"90px" }}/>
+               
+
+                <div className="middleteam">
+                    <h1>Looking for Web Application jobs...</h1>
+                    <h4>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam fugit at, dolor quia totam esse!</h4>
+                </div>
+
+                <div className="rightteam">
+                    <button>Learn About</button>
+                </div>
+
+                </div>
+
+                <div className="team2">
+                    <h1>Meet The Team</h1>
+                    <h4>Some sentence</h4>
+                </div>
+            </div>
+            {/* end */}
+            
+            <div className="referance">
+                <div className="item1">
+                <img className="leftteam" src="https://tse4.mm.bing.net/th?id=OIP.Ll8iCzMx3ipoSO6Btv-FpQHaEK&pid=Api&P=0&h=180" alt="react logo" style={{ width: '270px',height:"270px" }}/>
+
+                <h2>name 1</h2>
+                <h4><span>name1@gmail</span>.com</h4>
+                <h5>Lorem ipsum, dolor sit amet consectetur <br></br>adipisicing elit. Quam odio repellendus<br></br> laborum culpa quos </h5>
+                {/* icons */}
+                </div>
+
+                <div className="item2">
+                <img className="leftteam" src="https://tse2.mm.bing.net/th?id=OIP.2E0RuEBIm_cSt_35i7ccvwHaDt&pid=Api&P=0&h=180" alt="react logo" style={{ width: '270px',height:"270px" }}/>
+
+                <h2>name 1</h2>
+                <h4><span>name1@gmail</span>.com</h4>
+                <h5>Lorem ipsum, dolor sit amet consectetur <br></br>adipisicing elit. Quam odio repellendus<br></br> laborum culpa quos </h5>
+                {/* icons */}
+                </div>
+
+                <div className="item3">
+                <img className="leftteam" src="https://tse3.mm.bing.net/th?id=OIP.9baHoLFzxl9mlSY3AYZ-CQHaEK&pid=Api&P=0&h=180" alt="react logo" style={{ width: '270px',height:"270px" }}/>
+
+                <h2>name 1</h2>
+                <h4><span>name1@gmail</span>.com</h4>
+                <h5>Lorem ipsum, dolor sit amet consectetur <br></br>adipisicing elit. Quam odio repellendus<br></br> laborum culpa quos </h5>
+                {/* icons    */}
+                </div>
+
+            </div>
+            {/* end */}
+
+            <div className="imagecontent">
+                <div className="imgleft">
+                <img  src="https://tse3.mm.bing.net/th?id=OIP.9baHoLFzxl9mlSY3AYZ-CQHaEK&pid=Api&P=0&h=180" alt="react logo" style={{ width: '400px',height:"400px" }}/>
+                </div>
+                <div className="contentright">
+                    <div className="percentage1">
+                        <div >
+                            <h1>50 %</h1>
+                            <h4>Lorem ipsum, dolor sit amet <br></br>consectetur adipisicing elit.<br></br> Quam odio repellendus laborum </h4>
+                        </div>
+                        <div >
+                            <h1>17 %</h1>
+                            <h4>Lorem ipsum, dolor sit amet <br></br>consectetur adipisicing elit.<br></br> Quam odio repellendus laborum </h4>
+                        </div>
+                    </div>
+                    <div className="percentage2">
+                        <div >
+                            <h1>20%</h1>
+                            <h4>Lorem ipsum, dolor sit amet <br></br>consectetur adipisicing elit.<br></br> Quam odio repellendus laborum </h4>
+                        </div>
+                        <div >
+                            <h1>13%</h1>
+                            <h4>Lorem ipsum, dolor sit amet <br></br>consectetur adipisicing elit.<br></br> Quam odio repellendus laborum </h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="bluebox">
+                <h3>Lorem ipsum, dolor sit amet <br></br>consectetur adipisicing elit.<br></br> Quam odio repellendus </h3>
+            </div>
+            {/* end  */}
+            
+            <div className="payments">
+                <h2>Large payments volume or unique<br></br> business model?</h2>
+
+                <h5>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam odio repellendus laborum culpa quos atque officia possimus, aperiam labore neque facere quibusdam totam itaque corrupti et sunt cupiditate officiis rem?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam odio repellendus laborum culpa quos atque officia possimus, aperiam labore neque facere quibusdam totam itaque corrupti et sunt cupiditate officiis rem?
+                </h5>
+
+                <div className="cmplogo">
+                    <div className="row1">
+                    <img  src="https://assets.aboutamazon.com/88/05/0feec6ff47bab443d2c82944bb09/amazon-logo.png" alt="react logo" style={{ width: '100px',height:"100px" }}/>
+
+                    <img  src="https://assets.aboutamazon.com/88/05/0feec6ff47bab443d2c82944bb09/amazon-logo.png" alt="react logo" style={{ width: '100px',height:"100px" }}/>
+
+                    <img  src="https://assets.aboutamazon.com/88/05/0feec6ff47bab443d2c82944bb09/amazon-logo.png" alt="react logo" style={{ width: '100px',height:"100px" }}/>
+
+                    <img  src="https://assets.aboutamazon.com/88/05/0feec6ff47bab443d2c82944bb09/amazon-logo.png" alt="react logo" style={{ width: '100px',height:"100px" }}/>
+                    </div>
+                    <div className="row1">
+                    <img  src="https://assets.aboutamazon.com/88/05/0feec6ff47bab443d2c82944bb09/amazon-logo.png" alt="react logo" style={{ width: '100px',height:"100px" }}/>
+
+                    <img  src="https://assets.aboutamazon.com/88/05/0feec6ff47bab443d2c82944bb09/amazon-logo.png" alt="react logo" style={{ width: '100px',height:"100px" }}/>
+
+                    <img  src="https://assets.aboutamazon.com/88/05/0feec6ff47bab443d2c82944bb09/amazon-logo.png" alt="react logo" style={{ width: '100px',height:"100px" }}/>
+
+                    <img  src="https://assets.aboutamazon.com/88/05/0feec6ff47bab443d2c82944bb09/amazon-logo.png" alt="react logo" style={{ width: '100px',height:"100px" }}/>
+                    </div>
+                    <div className="row1">
+                    <img  src="https://assets.aboutamazon.com/88/05/0feec6ff47bab443d2c82944bb09/amazon-logo.png" alt="react logo" style={{ width: '100px',height:"100px" }}/>
+
+                    <img  src="https://assets.aboutamazon.com/88/05/0feec6ff47bab443d2c82944bb09/amazon-logo.png" alt="react logo" style={{ width: '100px',height:"100px" }}/>
+
+                    <img  src="https://assets.aboutamazon.com/88/05/0feec6ff47bab443d2c82944bb09/amazon-logo.png" alt="react logo" style={{ width: '100px',height:"100px" }}/>
+
+                    <img  src="https://assets.aboutamazon.com/88/05/0feec6ff47bab443d2c82944bb09/amazon-logo.png" alt="react logo" style={{ width: '100px',height:"100px" }}/>
+                    </div>
+                    
+                    
+                </div>
+            </div>
+            {/* end */}
+
+            <div className="price">
+                <h4>Pricing page</h4>
+                <h2>Small Pricing Plan For <br></br>
+                Your Creative Business</h2>
+
+                <div className="pricecontent">
+                    <div className="price1">
+                    <img src="https://assets.aboutamazon.com/88/05/0feec6ff47bab443d2c82944bb09/amazon-logo.png" style={{ width: '100px',height:"100px" }}></img>
+                    <h1>$0</h1>
+                    <h4>Price Range</h4>
+                    <br></br>
+                    <hr ></hr>
+                    <br></br>
+
+                    <h3 >
+                        Option 1 <br></br>
+                    Option 2 <br></br>
+                    Option 3 <br></br>
+                    Option 4 <br></br>
+                    </h3>
+
+                    <button>clickme</button>
+
+                    </div>
+
+
+                    <div className="price1">
+
+                    <img src="https://assets.aboutamazon.com/88/05/0feec6ff47bab443d2c82944bb09/amazon-logo.png" style={{ width: '100px',height:"100px" }}/>
+                    <h1>$29</h1>
+                    <h4>Price Range</h4>
+                    <br></br>
+                    <hr></hr>
+                    <br></br>
+
+                    <h3 >
+                        Option 1 <br></br>
+                    Option 2 <br></br>
+                    Option 3 <br></br>
+                    Option 4 <br></br>
+                    </h3>
+
+                    <button>clickme</button>
+                    </div>
+
+
+                    <div className="price1">
+                    <img src="https://assets.aboutamazon.com/88/05/0feec6ff47bab443d2c82944bb09/amazon-logo.png" style={{ width: '100px',height:"100px" }}/>
+                    <h1>$0</h1>
+                    <h4>Price Range</h4>
+                    <br></br>
+                    <hr></hr>
+                    <br></br>
+
+                    <h3 >
+                        Option 1 <br></br>
+                    Option 2 <br></br>
+                    Option 3 <br></br>
+                    Option 4 <br></br>
+                    </h3>
+
+                    <button>clickme</button>
+                    </div>
+                </div>
+            </div>
+
+            {/* end */}
+
+            <div className="footer">
+                <h1>Get Money Management and 
+                    <br></br>
+                    loan tips here!
+                </h1>
+                <div className="footer1">
+                    <div className="footleft">
+                        <input type="text" className="text" placeholder="Enter your name"></input>
+                        <input  type="submit" className="btn" placeholder="Enter your name"></input>
+                        {/* <button>Send</button> */}
+                        <br></br>
+                        <br></br>
+                        <h4>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam odio repellendus laborum culpa quos atque officia possimus, aperiam labore neque facere quibusdam totam itaque corrupti et sunt cupiditate officiis rem?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam odio repellendus laborum culpa quos atque officia possimus, aperiam labore neque facere quibusdam totam itaque corrupti et sunt cupiditate officiis rem?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam odio repellendus laborum culpa quos atque officia possimus, aperiam labore neque facere quibusdam totam itaque corrupti et sunt cupiditate officiis rem?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam odio repellendus laborum culpa quos atque officia possimus, aperiam labore neque facere quibusdam totam itaque corrupti et sunt cupiditate officiis rem?</h4>
+                        <br></br>
+                        <img src="https://cdn.pixabay.com/photo/2019/12/25/09/10/girl-mascot-4718135_640.png" style={{ width: '150px',height:"150px" }}/>
+                        <p>pirates of caribbean</p>
+                        
+                        
+                    </div>
+
+                    <div className="footright">
+                    <div className="footright1">
+                    <div>
+                    <img src="https://img.icons8.com/?size=48&id=118497&format=png" style={{ width: '100px',height:"100px" }}/>
+                    </div>
+                    <div>
+                    <img src="https://img.icons8.com/?size=48&id=13963&format=png" style={{ width: '100px',height:"100px" }}/>
+                    </div>
+
+                    <div>
+                    <img src="https://img.icons8.com/?size=48&id=32323&format=png" style={{ width: '100px',height:"100px" }}/>
+                    </div>
+
+
+                    </div>
+                    
+                    </div>
+                </div>
+            </div>
+            {/* end */}
+            <div className="last">
+                <h4>This is end of the Website</h4>
+                <h5>Thank yo!........</h5>
+            </div>
+            {/*  */}
+         </div>
+
+    )
+}
+
+export default App;
